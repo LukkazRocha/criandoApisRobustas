4eebfc14a2db38f0f0015c28386babe0d271dfdb
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-06-19 20:24:17 -0300

    consumindo os serviços

---
+++ XGame/XGame.AppConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using XGame.Domain.Arguments.Jogador;
+using XGame.Domain.Services;
+
+namespace XGame.AppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando ....");
+
+            var service = new ServiceJogador();
+            Console.WriteLine("Criei instancia do serviço");
+
+            AutenticarJogadorRequest request = new AutenticarJogadorRequest();
+            Console.WriteLine("Criei instancia do meu objeto request");
+            request.Email = "Lucas";
+
+            var response = service.AutenticarJogador(request);
+            Console.ReadKey();
+        }
+    }
+}
--- XGame/XGame.Domain/Services/ServiceJogador.cs
+++ XGame/XGame.Domain/Services/ServiceJogador.cs
@@ -9,6 +9,8 @@ namespace XGame.Domain.Services
     {
         private readonly IRepositoryJogador _repositoryJogador;
 
+        public ServiceJogador() { }
+
         public ServiceJogador(IRepositoryJogador repositoryJogador)
         {
             _repositoryJogador = repositoryJogador;
