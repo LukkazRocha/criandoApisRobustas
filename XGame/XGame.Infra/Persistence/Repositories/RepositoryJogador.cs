a98c8600d2fbabe03791500b41654c853cbb5bd3
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-06-24 16:31:01 -0300

    Implementando o repositorio

---
+++ XGame/XGame.Infra/Persistence/Repositories/RepositoryJogador.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using XGame.Domain.Entities;
+using XGame.Domain.Interfaces.Repositories;
+
+namespace XGame.Infra.Persistence.Repositories
+{
+    public class RepositoryJogador : IRepositoryJogador
+    {
+        #region Properties
+        private readonly XGameContext _context;
+        #endregion
+
+        #region Constructor
+        public RepositoryJogador(XGameContext context)
+        {
+            _context = context;
+        }
+        #endregion       
+
+        public Jogador AdicionarJogador(Jogador jogador)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AlterarJogador(Jogador jogador)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Jogador AutenticarJogador(string email, string senha)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Jogador> ListarJogador()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Jogador ObterJogadorPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
